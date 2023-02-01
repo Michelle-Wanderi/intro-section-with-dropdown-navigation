e28de2662efa4e2d509e1a6d24b61b5d5626f4e7
Michelle-Wanderi <michellewangari12@gmail.com>
2023-02-01 22:51:09 +0300

    updated

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,24 +1,64 @@
 import mobile from "./images/image-hero-mobile.png"
 import desktop from "./images/image-hero-desktop.png"
+import audiophile from "./images/client-audiophile.svg"
+import databiz from "./images/client-databiz.svg"
+import maker from "./images/client-maker.svg"
+import meet from "./images/client-meet.svg"
+
+
 function App() {
   return (
  <>
 
- <section className = "grid grid-cols-1 gap-10 md:grid-cols-2 md:place-items-center max-w-4xl lg:max-w-5xl md:mx-auto">
-  <article>
-      <h1>Make remote work</h1>
-      <p>
+ <section className = "flex flex-col-reverse py-20 md:grid md:gap-10 md:grid-cols-2 md:place-items-center max-w-4xl lg:max-w-5xl md:mx-auto">
+
+  <article className="text-center md:text-left px-5 xl:px-0 mt-10 md:mt-0">
+      <h1 className ="font-bold text-4xl lg:text-6xl xl:text-7xl mb-5">Make remote work</h1>
+      <p className="mb-5">
         Get your team in sync, no matter your location. Streamline processes, 
          create team rituals, and watch productivity soar.
     </p>
-    <button>Learn More</button>
+    <button className="bg-black rounded-lg shadow text-white font-bold
+      hover:opacity-75 transition-all duration-100 pt-3 pb-2 px-6 cursor-pointer
+    ">Learn More
+    </button>
+
+  <ul className="grid grid-cols-4 gap-1">
+    <li>
+      <img src={databiz} alt="" className="pt-5"/>
+    </li>
+
+    <li>
+    <img src={audiophile} alt=""className = "pt-5" />
+    </li>
+
+    <li>
+    <img src={meet} alt="" className = "pt-5" />
+    </li>
+
+
+    <li>
+    <img src={maker} alt="" className = "pt-5"/>
+    </li>
+    
+
+      
+     
+      
+      
+      </ul>
+
   </article>
-    <article>
+<article>
       <picture>
         <source media="(min-width: 768px)" srcSet={desktop} />
-        <img src={mobile} alt="" />
+        <img src={mobile} alt="" className="w-full" />
       </picture>
     </article>
+<article>
+
+
+</article>
  </section>
  </>
   );
